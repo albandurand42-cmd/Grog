8eeb3d21dffcfa2ba521f9b35033e4902c8d7079
albandurand42-cmd <albandurand42@gmail.com>
2026-08-14 09:06:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // Configuration globale de l'application.
 // Plus tard, ce fichier pourra contenir des constantes comme les URLs Spotify.
-
+export const SUPABASE_URL = 'https://yhkcgwyyfgjeudwowgwe.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_brkAboPJuyIWmPj5ebov3Q_uFQGcnvt';
+export const SPOTIFY_CLIENT_ID = 'd007448d677149d1a0828df50af6adeb';
 export const APP_NAME = 'Appli DJ';
